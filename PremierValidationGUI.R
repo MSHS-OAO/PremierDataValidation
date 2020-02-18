564945d44b5415cca612f21348e23a84f8a61112
armando-v <armando.villegas@mssm.edu>
2020-02-18 14:25:27 -0500

    Added GUI file

---
+++ PremierValidationGUI.R
@@ -0,0 +1,32 @@
+library(reticulate)
+library(taskscheduleR)
+library(shiny)
+library(miniUI)
+
+
+#py_run_file("PythonPremierValidation.py")
+#py_run_string("browser.quit()")
+#py_run_file("PremierBatchReports.py")
+#py_run_file("PremierValidTest.py")
+sites <- c("MSH","MSBI","MSSL","MSW")
+SelectedSites <- select.list(sites, graphics = T, multiple = T)
+if (length(SelectedSites) != 0){
+  write("Selected Sites:", file="PremierVariables.txt", append=T)
+  write(SelectedSites, file="PremierVariables.txt",append=TRUE)
+} else{
+  print("No site was selected")
+}
+
+Hour <- c("0:", "1:", "2:", "3:", "4:")
+Minute <- c("00" ,"05", "10","15" , "20")
+SelectedHour <-select.list(Hour, graphics = T, multiple = F)
+SelectedMinute <- select.list(Minute, graphics = T, multiple = F)
+CombinedTime <- paste0(SelectedHour,SelectedMinute)
+if (CombinedTime != ""){
+  write("Automation Time:", file ="PremierVariables.txt", append=T)
+  write(CombinedTime, file ="PremierVariables.txt", append=T)
+  } else{
+  print("No time was selected")
+}
+
+
